a751512b88b56c203c9a17b1ddfe73acc0e06686
LAPTOP-ACER <petkopenchovski@gmail.com>
2020-07-17 16:55:32 -0400

    add fragment in the navigation

--- app/src/main/java/com/example/dagger2example/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/dagger2example/fragments/HomeFragment.kt
@@ -1,6 +1,17 @@
 package com.example.dagger2example.fragments
 
+import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d(TAG,"is loaded")
+    }
+    companion object {
+        const val TAG = "HomeFragment"
+    }
 }
